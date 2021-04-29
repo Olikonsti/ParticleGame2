b9457c3b920b453368e7a7e2518dbc0ffcde8324
Olikonsti <68354546+Olikonsti@users.noreply.github.com>
2021-04-29 21:49:35 +0200

    Initial commit

---
+++ Particle.cpp
@@ -0,0 +1,153 @@
+#include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <string.h>
+#include <list>
+#include <iostream>
+#include <iterator> 
+using namespace std;
+
+int particle_size = 10;
+int kill_level = 1300 / particle_size;
+
+int particle_pos[200][200];
+
+/*
+
+0 Empty
+1 Particle
+2 Water
+
+
+
+*/
+
+
+
+class Particle {
+public:
+    int x;
+    int y;
+    int lastx, lasty;
+    string name;
+    string type;
+    sf::Color color;
+
+
+    Particle(int x_start, int y_start) {
+        type = "SOLID";
+        color = sf::Color::White;
+        x = x_start;
+        y = y_start;
+        lastx = x;
+        lasty = y;
+        particle_pos[x][y] = 1;
+        
+    };
+
+    void move(int x_neu, int y_neu) {
+        if (type == "SOLID") {
+
+        }
+        else if (type == "LIQUID" or type == "POWDER") {
+            particle_pos[x][y] = 0;
+            x += x_neu;
+            y += y_neu;
+            particle_pos[x][y] = 1;
+        }
+ 
+    }
+};
+
+class Sand : public Particle
+{
+public:
+    int x;
+    int y;
+
+    Sand(int x_start, int y_start) :Particle(x_start, y_start) {
+        type = "POWDER";
+        color = sf::Color::Color(255, 248, 199);
+    };
+};
+
+class Water : public Particle
+{
+public:
+    int x;
+    int y;
+
+    Water(int x_start, int y_start) :Particle(x_start, y_start) {
+        type = "LIQUID";
+        color = sf::Color::Blue;
+    };
+};
+
+class Stone : public Particle
+{
+public:
+    int x;
+    int y;
+
+    Stone(int x_start, int y_start):Particle(x_start, y_start) {
+        type = "SOLID";
+        color = sf::Color::Color(143, 143, 143);
+    };
+};
+
+
+list<Particle> particle_list;
+
+
+void spawn_particle(int x, int y, std::string type)
+{
+
+    // check if a particle is already at the same position
+    bool able_to_spawn = true;
+    for (auto i = particle_list.begin(); i != particle_list.end(); i++)
+    {
+        if (i->x == x && i->y == y) {
+            able_to_spawn = false;
+        }
+    }
+
+    // spawn particle if there is no particle at this x, y
+    if (able_to_spawn)
+    {
+        if (type == "TEST") {
+            Particle a(x, y);
+            particle_list.push_back(a);
+        }
+        else if (type == "Stone") {
+            Stone a(x, y);
+            particle_list.push_back(a);
+        }
+        else if (type == "Water") {
+            Water a(x, y);
+            particle_list.push_back(a);
+        }
+        else if (type == "Sand") {
+            Sand a(x, y);
+            particle_list.push_back(a);
+
+        }
+        else{
+            cout << "Unknown Element type " << type << "\n";
+        }
+    }
+    
+}
+
+
+void kill_particle(int x, int y)
+{
+    for (auto i = particle_list.begin(); i != particle_list.end(); i++)
+    {
+        
+        if (i->x == x && i->y == y)
+        {
+            particle_list.erase(i); 
+        }
+    }
+}
+
+
---
+++ ParticleGame2.cpp
@@ -0,0 +1,126 @@
+#include <SFML/Graphics.hpp>
+#include "Particle.cpp"
+#include <stdio.h>
+#include <string.h>
+#include <thread>
+
+int max_frame_rate = 120;
+int ticks = 0;
+bool game_paused = false;
+int mouse_hologram_border = 2;
+int mousex;
+int mousey;
+int gamex;
+int gamey;
+
+string selected_particle_type = "Stone";
+
+
+
+
+int main()
+{
+    sf::RectangleShape mouse_hologram;
+    mouse_hologram.setSize(sf::Vector2f(particle_size, particle_size)); 
+    mouse_hologram.setFillColor(sf::Color::Color(100, 200, 10, 0));
+    mouse_hologram.setOutlineColor(sf::Color::Color(100,200,10));
+    mouse_hologram.setOutlineThickness(mouse_hologram_border);
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "ParticleGame");// , sf::Style::Fullscreen);
+    window.setFramerateLimit(max_frame_rate);
+    sf::RectangleShape shape;
+
+    
+    
+    for (int i = 0; i < 20; i++) {
+        spawn_particle(10 + i, 20, "Stone");
+    }
+    
+    
+    
+
+    while (window.isOpen())
+    {
+        //spawn_particle(20, 10, "Water");
+        
+        ticks++;
+
+        // Window Events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Controls
+        sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
+        mousex = mouse_position.x;
+        mousey = mouse_position.y;
+        gamex = mousex / particle_size;
+        gamey = mousey / particle_size;
+
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            spawn_particle(gamex, gamey, selected_particle_type);
+            spawn_particle(gamex-1, gamey, selected_particle_type);
+            spawn_particle(gamex+1, gamey, selected_particle_type);
+        }
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+        {
+            kill_particle(gamex, gamey);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            selected_particle_type = "Water";
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            selected_particle_type = "Stone";
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            selected_particle_type = "Sand";
+        }
+        if (sf::Keyboard::Key (sf::Keyboard::Space))
+        {
+            game_paused = !game_paused;
+        }
+
+        // Console info print
+        if (ticks % 100 == 0) {
+            cout << "Particles: " << particle_list.size() << "\n";
+        }
+
+        
+            
+        // draw cycle
+        window.clear();
+        //draw mouse hologram
+
+        mouse_hologram.setPosition(gamex * particle_size, gamey * particle_size);
+
+        // draw all particles
+        for (auto i = particle_list.begin(); i != particle_list.end(); i++)
+        {
+            sf::RectangleShape shape(sf::Vector2f(particle_size, particle_size));
+            shape.setFillColor(i->color);
+
+            shape.setPosition((i->x * particle_size), (i->y * particle_size));
+
+            window.draw(shape);
+        }
+        window.draw(mouse_hologram);
+        window.display();
+            
+            
+        
+        
+        
+        
+        
+    }
+    return 0;
+}
